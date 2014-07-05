89681f8213d077daeba0b3a7bdcc0f441f6c5b57
dvberkel <daan.v.berkel.1980@gmail.com>
2014-07-05 10:31:32 +0200

    accept command line argument

--- stn.py
+++ stn.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+import sys
+
 def s(s0, s1):
     return 6 * s1 - s0
 
@@ -7,11 +9,13 @@ def t(t0, t1):
     return 6 * t1 - t0 + 2
 
 if __name__ == '__main__':
+    n = 20
+    if (len(sys.argv) == 2):
+        n = int(sys.argv[1])
     s0 = 0
     s1 = 1
     t0 = 0
     t1 = 1
-    n = 20
     while (n > 0):
         sn = s(s0, s1)
         s0 = s1
