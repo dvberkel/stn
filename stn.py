fe1809501a2a238a2e1a24bf2c1a782f35ad641a
dvberkel <daan.v.berkel.1980@gmail.com>
2014-07-05 10:23:54 +0200

    calculate square triangular numbers

--- stn.py
+++ stn.py
@@ -1,4 +1,10 @@
 #! /usr/bin/env python
 
+def s(s0, s1):
+    return 6 * s1 - s0
+
+def t(t0, t1):
+    return 6 * t1 - t0 + 2
+
 if __name__ == '__main__':
-    print 'Hello World'
+    print "{0},{1}".format(s(0, 1), t(0,1))
