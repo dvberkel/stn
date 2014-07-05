95f37f5a16c9716a03e2da5687076ef99c2f5189
dvberkel <daan.v.berkel.1980@gmail.com>
2014-07-05 10:27:44 +0200

    iterate solution

--- stn.py
+++ stn.py
@@ -7,4 +7,17 @@ def t(t0, t1):
     return 6 * t1 - t0 + 2
 
 if __name__ == '__main__':
-    print "{0},{1}".format(s(0, 1), t(0,1))
+    s0 = 0
+    s1 = 1
+    t0 = 0
+    t1 = 1
+    n = 20
+    while (n > 0):
+        sn = s(s0, s1)
+        s0 = s1
+        s1 = sn
+        tn = t(t0, t1)
+        t0 = t1
+        t1 = tn
+        print "{0},{1}".format(sn, tn)
+        n -= 1
